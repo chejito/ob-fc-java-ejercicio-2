ad6693c8604dc6a1cedf24fc469a1c426b2a1af3
chejito <smndz81@gmail.com>
2021-12-23 00:11:42 +0100

    Simplificación a 2 métodos.

--- src/main/java/UserAccessSystem.java
+++ src/main/java/UserAccessSystem.java
@@ -16,15 +16,23 @@ public class UserAccessSystem {
      */
     public boolean register(String email, String password) {
         if (!email.equals("") && !password.equals("")) {
-            User user = findUserByEmail(email);
-            if (user == null) {
-                int passwordHash = getHash(password);
+            User userToLogin = null;
+
+            for (User user : users) {
+                if (user.getEmail().equals(email)) {
+                    userToLogin = user;
+                }
+            }
+
+            if (userToLogin == null) {
+                int passwordHash = password.hashCode();
                 users.add(new User(email, passwordHash));
                 return true;
             }
         }
         return false;
     }
+
     /**
      * Método que permite iniciar sesión a un usuario con sus datos de usuario (e-mail y contraseña).
      *
@@ -33,41 +41,23 @@ public class UserAccessSystem {
      * @return int -1 si el e-mail no existe, -2 si la contraseña es incorrecta y 1 si ambos son correctos.
      */
     public int login(String email, String password) {
-        User user = findUserByEmail(email);
-        if (user == null) {
+        User userToLogin = null;
+
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                userToLogin = user;
+            }
+        }
+
+        if (userToLogin == null) {
             return -1;
         }
 
-        int hash = getHash(password);
-        if (hash != user.getPasswordHash()) {
+
+        int hash = password.hashCode();
+        if (hash != userToLogin.getPasswordHash()) {
             return -2;
         }
         return 1;
-    }
-    /**
-     * Método que devuelve un usuario almacenado en el ArrayList de usuarios.
-     * Lo busca por su e-mail.
-     *
-     * @param email E-mail del usuario a buscar.
-     * @return En caso afirmativo, el usuario. En caso negativo devuelve null.
-     */
-    public User findUserByEmail(String email) {
-        for (User user : users) {
-            if (user.getEmail().equals(email)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Método que genera un código hash a partir de la contraseña aportada como String.
-     *
-     * @param password String contraseña del usuario.
-     * @return int número entero que representa un código hash.
-     */
-    private int getHash(String password) {
-        String secret = "secret";
-        return password.hashCode() * secret.hashCode();
     }
 }
